86da0be203fe1c9edea76bf0930a91558956e713
-help <bubn0ff@ya.ru>
2019-03-23 19:13:26 +0300

    added the class Vector

--- js-game-1.4.1/game.js
+++ js-game-1.4.1/game.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// my graduent project in JS
+class Vector {
+	constructor(x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	plus(vector) {
+		if(!(vector instanceof Vector)) {
+			throw new Error('ћожно прибавл€ть к вектору только вектор типа Vector');
+		}
+		return new Vector(this.x + vector.x, this.y + vector.y);
+	}
+	
+	times(factor) {
+		return new Vector(this.x * factor, this.y * factor);
+	}
+}

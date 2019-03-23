0097b79a1923f5e2c8f215c4f4c4351ab5223c72
-help <bubn0ff@ya.ru>
2019-03-23 20:31:55 +0300

    changes in class - added constructor

--- js-game-1.4.1/game.js
+++ js-game-1.4.1/game.js
@@ -8,7 +8,7 @@ class Vector {
 	
 	plus(vector) {
 		if(!(vector instanceof Vector)) {
-			throw new Error('����� ���������� � ������� ������ ������ ���� Vector');
+			throw new Error('Можно прибавлять к вектору только вектор типа Vector');
 		}
 		return new Vector(this.x + vector.x, this.y + vector.y);
 	}
@@ -19,7 +19,21 @@ class Vector {
 }
 
 class Actor {
-	constructor() {
-
+	constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
+		if (!(pos instanceof Vector)){
+			throw new Error('Должно быть определено свойство pos, в котором размещён Vector');
+		}
+		
+		if (!(size instanceof Vector)){
+			throw new Error('Должно быть определено свойство size, в котором размещён Vector');
+		}
+		
+		if (!(speed instanceof Vector)){
+			throw new Error('Должно быть определено свойство speed, в котором размещён Vector');
+		}
+		
+		this.pos = pos;
+		this.size = size;
+		this.speed = speed;
 	}
 }
